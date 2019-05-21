450548ee0220c3dd1e4dbd623688c2ec2f7cda69
Glenn Dasai <glenn.dasai@gmail.com>
2019-05-21 16:13:43 +0200

    removed ValidateableResponse

--- src/test/java/com/cucumber/restapi/PersonDataService.java
+++ src/test/java/com/cucumber/restapi/PersonDataService.java
@@ -5,38 +5,34 @@ import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import io.restassured.response.Response;
-import io.restassured.response.ValidatableResponse;
 import io.restassured.specification.RequestSpecification;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.hasSize;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.*;
 
 
 public class PersonDataService extends BaseStepDef {
 
     private Response response;
-    public ValidatableResponse json;
     private RequestSpecification request;
     private static String API_ENDPOINT = "http://www.testautomationschool.nl/v1/test/cars.json";
 
 
     @Given("a person with the name (.*)")
-    public void aPersonWithName(String name){
+    public void aPersonWithName(String name) {
 //        request = given().param("q", "name:" + name);
         request = given();
     }
 
     @And("a the user is retrieved data in JSON format")
-    public void retrieveJsonPersonData(){
+    public void retrieveJsonPersonData() {
         response = request.when().get(API_ENDPOINT);
         System.out.println("response: " + response.prettyPrint());
     }
 
     @Then("the status code is (\\d+)")
     public void verify_status_code(int statusCode) {
-        json = response.then().statusCode(statusCode);
+        response.then().statusCode(statusCode);
     }
 
     @Then("^verify that element name has value \"([^\"]*)\"$")
